c397b403670116add3c7e6a027f369a908bdd5ce
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-12-19 12:39:44 -0500

    Created Home Grid for Later Components

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        <section class="text-green-400">Hello World</section>
+    <div class="grid grid-cols-2">
+        <div class="col-start-1"><section class="text-green-400">Hello World</section></div>
+        <div class="text-green-600 col-start-2">How You Been?</div>
     </div>
 </template>
 
