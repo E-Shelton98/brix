f70b5594835486be87feacc409a8987d25b10b43
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-12-19 12:52:17 -0500

    Header Component Init

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-green-300 h-16 absolute top-0 w-full">
+        <section>Header</section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheHeader'
+    }
+</script>
--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,4 +1,5 @@
 <template>
+    <TheHeader />
     <div class="grid grid-cols-2">
         <div class="col-start-1"><section class="text-green-400">Hello World</section></div>
         <div class="text-green-600 col-start-2">How You Been?</div>
@@ -6,7 +7,12 @@
 </template>
 
 <script>
+    import TheHeader from './TheHeader'
+    
     export default {
-        name: 'TheHome'
+        name: 'TheHome',
+        components: {
+            TheHeader
+        }
     }
 </script>
