8edce4dbb8e9739a584c01b36392a107747bdc32
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-12-19 12:33:40 -0500

    TheHome.vue init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheHome />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHome from './components/TheHome'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHome
   }
 }
 </script>
---
+++ src/components/TheHome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <section class="text-green-400">Hello World</section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheHome'
+    }
+</script>
