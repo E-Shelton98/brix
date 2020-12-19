1adee91185c9172f72b1ccd0956d1bb26bc9de3d
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-12-19 15:11:09 -0500

    Generator component can affect Mock Component

---
+++ src/components/TheMock.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="grid grid-rows-16 grid-cols-12 h-full">
+        <div class="bg-blue-400 col-start-1 col-span-12" v-if="isTrue">
+            <section>Hello World</section>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheMock',
+        props: {
+            isTrue: Boolean
+        },
+        data() {
+            return {
+                displayHeader: this.isTrue
+            }
+        }
+    }
+</script>
